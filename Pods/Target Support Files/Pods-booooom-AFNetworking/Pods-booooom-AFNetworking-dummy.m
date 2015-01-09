e2f38f89a15e2c21f339a15548aee9238689dc2b
Masamitsu Konya <masamitsu.konya@gmail.com>
2015-01-09 10:56:51 +0900

    remove pod

--- Pods/Target Support Files/Pods-booooom-AFNetworking/Pods-booooom-AFNetworking-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_booooom_AFNetworking : NSObject
-@end
-@implementation PodsDummy_Pods_booooom_AFNetworking
-@end
--- Pods/Target Support Files/Pods-booooom/Pods-booooom-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_booooom : NSObject
-@end
-@implementation PodsDummy_Pods_booooom
-@end
