55b7c92657f4cdc21111b19dec0779a549dde756
Masamitsu Konya <masamitsu.konya@gmail.com>
2015-01-09 10:42:35 +0900

    ナビゲーションコントローラーの追加

---
+++ Pods/Target Support Files/Pods-booooom-AFNetworking/Pods-booooom-AFNetworking-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_booooom_AFNetworking : NSObject
+@end
+@implementation PodsDummy_Pods_booooom_AFNetworking
+@end
---
+++ Pods/Target Support Files/Pods-booooom/Pods-booooom-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_booooom : NSObject
+@end
+@implementation PodsDummy_Pods_booooom
+@end
