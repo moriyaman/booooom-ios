db11540e3e653558f2e478c421ac98d5facc1fe6
Yuki Moriyama <yuki.moriyaman@gmail.com>
2015-01-09 10:26:59 +0900

    add bridge header

---
+++ booooom/booooom-Bridging-Header.h
@@ -0,0 +1,12 @@
+//
+//  booooom-Bridging-Header.h
+//  booooom
+//
+//  Created by Yuki Moriyama on 2015/01/09.
+//  Copyright (c) 2015年 yuki.moriyama. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <AFNetworking/AFNetworking.h>
+#import <SVProgressHUD.h>
+#import <FacebookSDK/FacebookSDK.h>
