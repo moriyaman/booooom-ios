e9478f44822b5d6d1a59ac9cacf601a3785a7c35
Masamitsu Konya <masamitsu.konya@gmail.com>
2015-01-09 14:41:17 +0900

    詳細ページとswiftファイルを紐付ける

---
+++ booooom/DetailViewController.swift
@@ -0,0 +1,19 @@
+//
+//  DetailViewController.swift
+//  booooom
+//
+//  Created by 紺谷 昌充 on 2015/01/09.
+//  Copyright (c) 2015年 yuki.moriyama. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+
+}
+
+
+
+
+
